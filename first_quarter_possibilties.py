47330170c6082bc97ebd53bb021ad654c485c7f8
D <techtalkwithd@gmail.com>
2023-05-07 18:39:55 -0500

    Bug fix

--- first_quarter_possibilties.py
+++ first_quarter_possibilties.py
@@ -6,20 +6,7 @@ players = ["Jokic", "Murray", "Porter", "Booker", "Durant", "Ayton", "Points"]
 decisions = ["O", "U"]
 
 # Get user input for whether to randomize output or not
-while True:
-    randomize_output = input("Do you want to randomize the output? (y/n): ").lower()
-    if randomize_output == "y" or randomize_output == "n":
-        break
-
-randomize_output = randomize_output == "y"
-
-# Get user input for whether to output HTML table or not
-while True:
-    output_html = input("Do you want to output an HTML table? (y/n): ").lower()
-    if output_html == "y" or output_html == "n":
-        break
-
-output_html = output_html == "y"
+randomize_output = input("Do you want to randomize the output? (y/n): ").lower() == "y"
 
 # Generate all possible combinations of decisions for each player
 combos = list(product(decisions, repeat=len(players)))
@@ -30,34 +17,31 @@ if randomize_output:
 
 total_combos = len(combos)
 
-# Initialize the output string
-if output_html:
-    output = "<table>\n<tr><th>Players</th><th>Decision</th></tr>\n"
-else:
-    output = "| Players | Decision |\n| ------- | -------- |\n"
+# Add header row to the table
+output = "<table style='border-collapse: collapse;'>\n<tr style='background-color: #f2f2f2;'><th>Selection</th><th>Players</th><th>Decision</th></tr>\n"
 
 # Add each possible combination as a row in the table
-for combo in combos:
-    if output_html:
-        rows = ["<tr><td>{}</td><td>{}</td></tr>\n".format(player, decision) for player, decision in zip(players, combo)]
-        output += "".join(rows)
-    else:
-        rows = ["| {} | {} |\n".format(player, decision) for player, decision in zip(players, combo)]
-        output += "".join(rows) + "| ------- | -------- |\n"
+for i, combo in enumerate(combos):
+    rows = ["<td><input type='checkbox' name='selection' value='{0}'></td>".format(i + 1)]
+    rows += ["<td>{0}</td><td>{1}</td>".format(player, decision) for player, decision in zip(players, combo)]
+    output += "<tr style='border: 1px solid black;'>{0}</tr>\n".format("".join(rows))
+
+    # Add separator every 10 rows
+    if i % 10 == 9:
+        output += "<tr style='border: none; height: 10px;'></tr>\n"
+
+# Close the table
+output += "</table>"
 
 # Get output file name from user
-output_filename = input("Enter the name of the output file (without extension): ")
+output_filename = input("Enter the name of the output file (without .html extension): ")
 
 # Create 2023 directory if it doesn't exist
 if not os.path.exists("2023"):
     os.mkdir("2023")
 
 # Save output to file in 2023 directory
-if output_html:
-    output_filepath = os.path.join("2023", output_filename + ".html")
-else:
-    output_filepath = os.path.join("2023", output_filename + ".md")
-
+output_filepath = os.path.join("2023", output_filename + ".html")
 with open(output_filepath, "w") as f:
     f.write(output)
 
